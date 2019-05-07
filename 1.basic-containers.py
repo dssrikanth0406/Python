2619445fd33ad99ae6c1da395e6dcda9d3b21d9f
dssrikanth0406 <50256936+dssrikanth0406@users.noreply.github.com>
2019-05-07 18:48:06 +0530

    Add files via upload

---
+++ 1.basic-containers.py
@@ -0,0 +1,16 @@
+#understanding basic types
+#types are inferred by runtime automatically
+
+a = 10
+print(a)
+print(type(a))
+
+b = 13.6
+print(type(b))
+
+c = 'abc'
+print(type(c))
+
+e = False 
+print(e)
+print(type(e))
